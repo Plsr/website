fdfcac14dd80858c2de9f01a6d65edc4554c54a5
Plsr <christianpoplawski@gmail.com>
2022-02-12 16:54:03 +0100

    Auto-Generate tag pages

--- _plugins/generate_tags.rb
+++ _plugins/generate_tags.rb
@@ -2,10 +2,37 @@ module GenerateTags
   class TagPageGenerator < Jekyll::Generator
     safe true
 
+    def existing_tag_pages
+      Dir.entries("_tags").map { |f| f.match(/(.*).md/) }.compact.map{ |t| t[1] }
+    end
+
+    def tags_on_posts(posts)
+      posts.docs.map{ |p| p.data["tags"]}.flatten.uniq
+    end
+
     def generate(site)
-      pp "i ran"
-      site.posts.each do |post|
-        pp post.tags
+      tags_on_posts(site.posts).each do |tag|
+        site.pages << TagPage.new(site, tag) unless existing_tag_pages.include?(tag)
+      end
+    end
+
+    def generate_tag_file(tag)
+      File.open("_tags/#{tag}.md", "wb") do |file|
+        file << "---\ntag-name: #{tag}\nlayout: tag\n---\n"
+      end
+    end
+
+    class TagPage < Jekyll::Page
+      def initialize(site, tag)
+        @site = site
+        @dir = 'tags'
+        @basename = tag
+        @ext = '.html'
+        @name = "#{@basename}#{@extension}"
+        @data = {
+          'tag-name' => tag,
+          'layout' => 'tag'
+        }
       end
     end
   end
--- _tags/web-dev.md
+++
@@ -1,4 +0,0 @@
----
-tag-name: web-dev
-layout: tag
----
